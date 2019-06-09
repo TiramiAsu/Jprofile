403ec41c1b150b9d46f205a7e715228007937cde
TiramiAsu <46002098+TiramiAsu@users.noreply.github.com>
2019-06-09 17:33:12 +0800

    refresh sql

--- src/main/java/c99_web/model/sql/CreateTableData_Products.sql
+++ src/main/java/c99_web/model/sql/CreateTableData_Products.sql
@@ -3,8 +3,7 @@
  * Created: Jun 4, 2019
  */
 CREATE TABLE ICProducts(
-    pd_id integer primary key generated always as identity, -- 主鍵(產品代號)
-    pd_code varchar(10) not null,               -- (編號)
+    pd_code varchar(10) primary key, -- 主鍵(產品代號)
     pd_name varchar(20) not null,               -- (名稱)
     pd_price integer not null,                  -- (價錢)
     pd_cost integer not null,                   -- (成本)
@@ -20,13 +19,12 @@ CREATE TABLE ICProducts(
 --        ('A002', '薯條', 30, 10, '包', '停售', '', DEFAULT);
 
 INSERT INTO JAVAWEB.ICPRODUCTS (PD_CODE, PD_NAME, PD_PRICE, PD_COST, PD_UNIT, PD_STATUS, PD_REMARK) 
- VALUES ('A001', '漢堡', 50, 22, '個', '販賣中', '賺'),
-        ('A002', '薯條', 30, 10, '包', '販賣中', '賺翻了'),
-        ('A003', '雪碧', 40, 5, '杯', '販賣中', '賺翻了個大白眼'),
-        ('A004', '可樂', 30, 15, '杯', '停售', '沒什麼好說的'),
-        ('K852', '蘋果派', 60, 19, '個', '停售', '蘋果沒了'),
-        ('A007', '黑漢堡', 79, 40, '個', '停售', ''),
-        ('F085', '冰炫風', 45, 16, '杯', '販售中', ''),
-        ('D852', '薯餅', 40, 10, '個', '販賣中', '現做');
-
+ VALUES ('A001', '漢堡', 50, 22, '個', 'Y', '賺'),
+        ('A002', '薯條', 30, 10, '包', 'Y', '賺翻了'),
+        ('A003', '雪碧', 40, 5, '杯', 'Y', '賺翻了個大白眼'),
+        ('A004', '可樂', 30, 15, '杯', 'N', '沒什麼好說的'),
+        ('K852', '蘋果派', 60, 19, '個', 'N', '蘋果沒了'),
+        ('A007', '黑漢堡', 79, 40, '個', 'N', ''),
+        ('F085', '冰炫風', 45, 16, '杯', 'Y', ''),
+        ('D852', '薯餅', 40, 10, '個', 'Y', '現做');
 
